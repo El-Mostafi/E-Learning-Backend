2068fd1ffb1da811b2073bf199a1ff5fccf41797
BH-Hamza <hamza.boutlih@gmail.com>
2025-02-06 01:23:47 +0100

    Just formatted code

--- common/src/middllewares/require-auth.ts
+++ common/src/middllewares/require-auth.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { NotAutherizedError } from "../../../common";
 
-export const requireAuth= async (req: Request, res: Response, next: NextFunction) => {
-    if(!req.currentUser || req.currentUser.emailConfirmed == false){
-        return next(new NotAutherizedError());
-    }
-    next();
-}
+export const requireAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser || req.currentUser.emailConfirmed == false) {
+    return next(new NotAutherizedError());
+  }
+  next();
+};
