6b4721ab0fddcfb27064555043375e2846beacca
MOHAMED EL MOSTAFI <elmoustafimohammed@gmail.com>
2025-07-26 19:51:29 +0100

    add setting for deploy the app on vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { AppModule } from './module';
 import express  from 'express';
-const bootstrap=()=>{
-    const app = new AppModule(express());
-    app.start();
-}
 
-bootstrap();
+const app = express();
+const appModule = new AppModule(app);
+
+appModule.start().catch(console.error); 
+
+export default app; 
